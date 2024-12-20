fccc168ebbd7c47732467e5992f0ad896f7f5131
Gabriel Villacis <55613545+gabvillacis@users.noreply.github.com>
2024-12-20 17:01:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
   return (
     <main className="board">
-      <h1>Tic tac toe</h1>
+      <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {board.map((square, index) => {
